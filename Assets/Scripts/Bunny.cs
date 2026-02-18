f8c3eed2f9359e03e3b633aeff6d17a5c6ba6a1c
sketchsoh <sketchsoh@gmail.com>
2026-02-18 22:08:20 +0800

    Bunnies are draggable

---
+++ Assets/Scripts/Bunny.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class Bunny : MonoBehaviour
+{
+    private const float minXBounds = -0.4f;
+    private const float maxXBounds = 6.5f;
+    private const float minYBounds = -1.15f;
+    private const float maxYBounds = 3f;
+    
+    private BunnyManager bunnyManager;
+    
+    [Header("Bunny Parts")]
+    [SerializeField] public SpriteRenderer bunnyTail;
+    [SerializeField] public SpriteRenderer bunnyBody;
+    [SerializeField] public SpriteRenderer bunnyHead;
+    [SerializeField] public SpriteRenderer bunnyEars;
+
+    [Header("Bunny Stats")]
+    [SerializeField] public float cutenessStat;
+    [SerializeField] public float playfulnessStat;
+    [SerializeField] public float friendlinessStat;
+    
+    [Header("Other Stats")]
+    [SerializeField] public string bunnyName;
+    [SerializeField] public float bunnyAge;
+    [SerializeField] public BunnyType bunnyType;
+    [SerializeField] public Color bunnyColor;
+    
+    private bool held = false;
+    
+    public void InitializeBunny(
+        string bunnyName, 
+        BunnyType type, 
+        Color color,
+        Sprite Tail, 
+        Sprite Body, 
+        Sprite Head, 
+        Sprite Ears, 
+        float cuteness, 
+        float playfulness, 
+        float friendliness
+        )
+    {
+        bunnyColor = color;
+        bunnyTail.sprite = Tail;
+        bunnyTail.color = bunnyColor;
+        bunnyBody.sprite = Body;
+        bunnyBody.color = bunnyColor;
+        bunnyHead.sprite = Head;
+        bunnyHead.color = bunnyColor;
+        bunnyEars.sprite = Ears;
+        bunnyEars.color = bunnyColor;
+        cutenessStat = cuteness;
+        playfulnessStat = playfulness;
+        friendlinessStat = friendliness;
+        this.bunnyName = bunnyName;
+        gameObject.name = bunnyName;
+        bunnyAge = 0;
+        bunnyType = type;
+        held = false;
+        bunnyManager = FindAnyObjectByType<BunnyManager>();
+    }
+
+    private void Update()
+    {
+        if (held)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePos;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        held = true;
+    }
+
+    private void OnMouseUp()
+    {
+        held = false;
+    }
+}
--- Assets/Scripts/BunnyManager.cs
+++ Assets/Scripts/BunnyManager.cs
@@ -11,6 +11,47 @@ public class BunnyManager : MonoBehaviour
     [SerializeField] private Slider bunnyCuteness;
     [SerializeField] private Slider bunnyPlayfulness;
     [SerializeField] private Slider bunnyFriendliness;
+    [SerializeField] private int startingBunnyCount = 5;
+
+    [Header("Bunny Sprites")]
+    [Header("Gradient")]
+    [SerializeField] private Sprite[] gradientHeadSprites;
+    [SerializeField] private Sprite[] gradientBodySprites;
+    [SerializeField] private Sprite[] gradientEarSprites;
+    [SerializeField] private Sprite[] gradientTailSprites;
+    [Space]
+    [Header("Normal")]
+    [SerializeField] private Sprite[] normalHeadSprites;
+    [SerializeField] private Sprite[] normalBodySprites;
+    [SerializeField] private Sprite[] normalEarSprites;
+    [SerializeField] private Sprite[] normalTailSprites;
+    [Space]
+    [Header("Spotted")]
+    [SerializeField] private Sprite[] spottedHeadSprites;
+    [SerializeField] private Sprite[] spottedBodySprites;
+    [SerializeField] private Sprite[] spottedEarSprites;
+    [SerializeField] private Sprite[] spottedTailSprites;
+
+    [SerializeField] private GameObject bunnyPrefab;
+    
+    private const float minXBounds = -0.4f;
+    private const float maxXBounds = 6.5f;
+    private const float minYBounds = -1.15f;
+    private const float maxYBounds = 3f;
+
+    void Start()
+    {
+        GenerateStartingBunnies();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateBunny();
+        }
+    }
+    
 
     public void ShowStatsPanel(string name, float cutenessStat, float playfulnessStat, float affectionStat)
     {
@@ -25,4 +66,55 @@ public class BunnyManager : MonoBehaviour
     {
         statsPanel.SetActive(false);
     }
+
+    public void GenerateBunny()
+    {
+        int rand =  Random.Range(0, 3);
+        BunnyType selectedBunnyType = (BunnyType)rand;
+        Sprite bunnyHead = null;
+        Sprite bunnyBody = null;
+        Sprite bunnyEar =  null;
+        Sprite bunnyTail =  null;
+        switch (selectedBunnyType)
+        {
+            case BunnyType.Normal:
+                bunnyHead = normalHeadSprites[Random.Range(0, normalHeadSprites.Length)];
+                bunnyBody = normalBodySprites[Random.Range(0, normalBodySprites.Length)];
+                bunnyEar = normalEarSprites[Random.Range(0, normalEarSprites.Length)];
+                bunnyTail = normalTailSprites[Random.Range(0, normalTailSprites.Length)];
+                break;
+            case BunnyType.Spotted:
+                bunnyHead = spottedHeadSprites[Random.Range(0, normalHeadSprites.Length)];
+                bunnyBody = spottedBodySprites[Random.Range(0, spottedBodySprites.Length)];
+                bunnyEar = spottedEarSprites[Random.Range(0, spottedEarSprites.Length)];
+                bunnyTail = spottedTailSprites[Random.Range(0, spottedTailSprites.Length)];
+                break;
+            case BunnyType.Gradient:
+                bunnyHead = gradientHeadSprites[Random.Range(0, gradientHeadSprites.Length)];
+                bunnyBody = gradientBodySprites[Random.Range(0, gradientBodySprites.Length)];
+                bunnyEar = gradientEarSprites[Random.Range(0, gradientEarSprites.Length)];
+                bunnyTail = gradientTailSprites[Random.Range(0, gradientTailSprites.Length)];
+                break;
+        }
+
+        Color bunnyColor = Random.ColorHSV(0.0f, 1.0f, 0.3f, 0.75f, 0.75f, 1.0f);
+        Bunny newBunny = Instantiate(bunnyPrefab, transform.position, Quaternion.identity).GetComponent<Bunny>();
+        newBunny.InitializeBunny("Test", selectedBunnyType, bunnyColor, bunnyTail, bunnyBody, bunnyHead, bunnyEar, 50f, 50f, 50f);
+        newBunny.transform.position = new Vector3(Random.Range(minXBounds, maxXBounds), Random.Range(minYBounds, maxYBounds));
+    }
+
+    private void GenerateStartingBunnies()
+    {
+        for (int i = 0; i < startingBunnyCount; i++)
+        {
+            GenerateBunny();
+        }
+    }
+}
+
+public enum BunnyType
+{
+    Gradient,
+    Normal,
+    Spotted
 }
